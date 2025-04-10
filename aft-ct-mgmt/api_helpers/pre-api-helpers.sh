cffa8aaf318689dad07c849959803a529f9a0639
huytng <huytng@U-3T9RX5UYV0LEP.corp.amazonworkspaces.com>
2025-04-10 13:33:46 -0700

    test

---
+++ aft-ct-mgmt/api_helpers/pre-api-helpers.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Copyright Amazon.com, Inc. or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+
+echo "Executing Pre-API Helpers"
+LANDING_ZONE_ID=$(aws controltower list-landing-zones --query 'landingZones.arn' --output text)
+
+export LANDING_ZONE_ID=$LANDING_ZONE_ID
