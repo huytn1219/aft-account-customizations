a659399e253ae118ee824553a76070555794dc34
huytng <huytng@U-3T9RX5UYV0LEP.corp.amazonworkspaces.com>
2025-04-14 10:20:32 -0700

    update reset

--- aft-ct-mgmt/api_helpers/post-api-helpers.sh
+++ aft-ct-mgmt/api_helpers/post-api-helpers.sh
@@ -4,3 +4,32 @@
 #
 
 echo "Executing Post-API Helpers"
+
+echo "Update Accounts"
+
+# Get landing zone ID from SSM
+LZ_ID=$(aws ssm get-parameter --name "/landingzone_id" --query 'Parameter.Value' --output text)
+echo "Landing Zone ID: $LZ_ID"
+
+# Get root OU ID
+ROOT_ID=$(aws organizations list-roots --query 'Roots[0].Id' --output text)
+
+# Get all OUs
+OUS=$(aws organizations list-organizational-units-for-parent \
+  --parent-id "$ROOT_ID" \
+  --query 'OrganizationalUnits[*].Id' --output text)
+
+# Loop through OUs
+for OU_ID in $OUS; do
+  echo "Processing OU: $OU_ID"
+
+  #  Get the ARN of the target OU to re-register.
+  OU_ARN=$(aws organizations describe-organizational-unit --organizational-unit-id $OU_ID --query 'OrganizationalUnit.[Arn]' --output text)
+
+  # Get the ARN of the EnabledBaseline resource for the target OU.
+  echo "Found BASELINE_ARN for $OU_ARN"
+  BASELINE_ARN=$(aws controltower list-enabled-baselines --query 'enabledBaselines[?targetIdentifier==`'$OU_ARN'`].[arn]' --output text)
+
+  # Reset the Enabled Baseline
+  echo "Resetting Enabled Baseline."
+  aws controltower reset-enabled-baseline --enabled-baseline-identifier $BASELINE_ARN
--- aft-ct-mgmt/api_helpers/pre-api-helpers.sh
+++ aft-ct-mgmt/api_helpers/pre-api-helpers.sh
@@ -6,4 +6,3 @@
 echo "Executing Pre-API Helpers"
 LANDING_ZONE_ID=$(aws controltower list-landing-zones --query 'landingZones[0].arn' --output text)
 aws ssm put-parameter --name "landingzone_id" --value $LANDING_ZONE_ID --type "String"
-terraform force-unlock -force 76405403-948a-fb64-a1be-9723e0dc368b
