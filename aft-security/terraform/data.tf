d988a5d26413c6e8a3190e26a9767c0b5b87b12e
huytng <huytng@U-3T9RX5UYV0LEP.corp.amazonworkspaces.com>
2025-04-14 21:12:31 -0700

    security

--- aft-security/terraform/data.tf
+++
@@ -1,8 +0,0 @@
-# Copyright Amazon.com, Inc. or its affiliates. All rights reserved.
-# SPDX-License-Identifier: Apache-2.0
-
-data "aws_caller_identity" "current" {}
-
-data "aws_ssm_parameter" "landing_zone_id" {
-    name = "landingzone_id"
-}
