164248b95d5e590f6050f11e095875f99e0f16f4
Huy Nguyen <61076435+huytn1219@users.noreply.github.com>
2025-05-10 15:47:55 -0700

    Update main.tf

--- aft-ct-mgmt/terraform/main.tf
+++ aft-ct-mgmt/terraform/main.tf
@@ -3,6 +3,15 @@ resource "aws_controltower_landing_zone" "main" {
   version       = "3.3"
 }
 
+# Trigger OU re-registration process to update accounts
+resource "null_resource" "run_script" {
+  provisioner "local-exec" {
+    command = "./re-register-ous.sh"
+    environment = { BASELINE_VERSION = var.baseline_version }
+  }
+  depends_on = [aws_controltower_landing_zone.main]
+}
+
 import {
   to = aws_controltower_landing_zone.main
   id = data.aws_ssm_parameter.landing_zone_id.value
