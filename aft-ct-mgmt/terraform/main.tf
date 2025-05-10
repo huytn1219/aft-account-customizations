e21c6aaedcbd349bc402d674fa098462deb7f92f
Huy Nguyen <61076435+huytn1219@users.noreply.github.com>
2025-05-10 15:49:14 -0700

    Update main.tf

--- aft-ct-mgmt/terraform/main.tf
+++ aft-ct-mgmt/terraform/main.tf
@@ -12,6 +12,11 @@ resource "null_resource" "run_script" {
   depends_on = [aws_controltower_landing_zone.main]
 }
 
+variable baseline_version {
+  value       = "4.0"
+  description = "Baseline version of the Control Tower"
+}
+
 import {
   to = aws_controltower_landing_zone.main
   id = data.aws_ssm_parameter.landing_zone_id.value
