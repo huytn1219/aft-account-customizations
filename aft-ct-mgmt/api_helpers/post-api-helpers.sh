f6f3d75471269fb7427217fecd4c8b9bdf81cb95
Huy Nguyen <61076435+huytn1219@users.noreply.github.com>
2025-05-11 12:58:17 -0700

    Update post-api-helpers.sh

--- aft-ct-mgmt/api_helpers/post-api-helpers.sh
+++ aft-ct-mgmt/api_helpers/post-api-helpers.sh
@@ -1,1 +1,27 @@
+# © 2025 Amazon Web Services, Inc. or its affiliates. All Rights Reserved.
+#
+# This AWS Content is provided subject to the terms of the AWS Customer Agreement available at
+# http://aws.amazon.com/agreement or other written agreement between Customer and either
+# Amazon Web Services, Inc. or Amazon Web Services EMEA SARL or both.
+#!/bin/bash
 
+echo "Executing Post-API Helpers"
+echo "---------------------"
+echo "---------------------"
+echo "---------------------Change AWS_PROFILE=AFT-TARGET-admin---------------------"
+export AWS_PROFILE=aft-target
+echo "aws sts get-caller-identity"
+aws sts get-caller-identity
+echo "aws sts get-caller-identity"
+echo "---------------------"
+echo "---------------------"
+echo "---------------------Access folder: CD \API_Helpers---------------------"
+cd $DEFAULT_PATH/$CUSTOMIZATION/api_helpers/
+echo "---------------------Done!---------------------"
+echo "---------------------"
+echo "---------------------"
+echo "---------------------Executing ./enable_ct_region.py---------------------"
+python3 ./enable_ct_region.py
+echo "---------------------Done!---------------------"
+echo "---------------------"
+echo "---------------------"
