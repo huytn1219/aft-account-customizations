770084bacfabaa0da7be696eecc6c49aa89fb2e3
Huy Nguyen <61076435+huytn1219@users.noreply.github.com>
2025-05-10 18:10:48 -0700

    Update main.tf

--- aft-ct-mgmt/terraform/main.tf
+++ aft-ct-mgmt/terraform/main.tf
@@ -1,6 +1,48 @@
+locals {
+  identifiers = {
+      region = "us-west-1"
+      additional_regions = []
+}
+
+variables "log_retention_days" {
+     value = 365
+}
+
+variables "access_log_retention_days" {
+     value = 3650
+}
+
 resource "aws_controltower_landing_zone" "main" {
-  manifest_json = file("${path.module}/LandingZoneManifest.json")
-  version       = "3.3"
+  manifest_json = jsonencode({
+      governedRegions = concat([local.identifiers.region], local.identifiers.additional_regions)
+      organizationStructure = {
+        security = {
+          name = "Security"
+        }
+        sandbox = {
+          name = "Sandbox"
+        }
+      }
+      centralizedLogging = {
+            accountId = data.aws_ssm_parameter.aft_logging_account_id.value
+            configurations = {
+              loggingBucket = {
+                retentionDays = tostring(var.log_retention_days)
+              }
+              accessLoggingBucket = {
+                retentionDays = tostring(var.access_log_retention_days)
+              }
+            }
+            enabled = true
+          }
+          securityRoles = {
+            accountId = data.aws_ssm_parameter.aft_audit_account_id.value
+          }
+          accessManagement = {
+            enabled = false
+          }
+      })
+  version       = var.landingzone_version
 }
 
 # Trigger OU re-registration process to update accounts
@@ -14,4 +56,12 @@ resource "null_resource" "run_script" {
 import {
   to = aws_controltower_landing_zone.main
   id = data.aws_ssm_parameter.landing_zone_id.value
+}
+
+data "aws_ssm_parameter" "aft_audit_account_id" {
+  name = "/aft/account/audit/account-id"
+}
+
+data "aws_ssm_parameter" "aft_logging_account_id" {
+  name = "/aft/account/log-archive/account-id"
 }
